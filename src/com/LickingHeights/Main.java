9a0625be335e9b28cd3b05dc1b14cfa461b0e37a
Rebecca <Rebecca.powers.2021@student.lhschools.org>
2020-03-03 14:12:13 -0500

    UserDataCaptured

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,24 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    }
+    public static void ModulusClock(){
+        int hour;
+        int minute;
+        Scanner keyboard;
+        keyboard=Scanner(System.in);
+
+        System.out.println("Please type the hour");
+        hour=keyboard.nextInt();
+
+        System.out.println("please type the minutes");
+        minute=keyboard.nextInt();
+
+
     }
 }
